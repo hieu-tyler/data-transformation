4c60b89ab2310760d40a1eb65e87a9c51bfdecd6
tyler <minhhieu.ha.2520@gmail.com>
2024-03-27 16:29:12 +1100

    [IMP] Improve data decoder

--- dynamic_data/views.py
+++ dynamic_data/views.py
@@ -1,13 +1,17 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from django.http import JsonResponse
+from django.apps import apps
+from django.db import models
+import csv
+import pandas as pd
+from datetime import datetime
+from dateutil import parser
+import re
 
 from .models import Student
 from .serializers import *
-from django.http import JsonResponse
-import csv
-from datetime import datetime
-
 
 @api_view(['GET', 'POST'])
 def students_list(request):
@@ -48,24 +52,54 @@ def students_detail(request, pk):
 @api_view(['POST'])
 def upload_csv(request):
     if request.method == 'POST' and request.FILES['file']:
-        csv_file = request.FILES['file']
-        decoded_file = csv_file.read().decode('utf-8').splitlines()
+        file = request.FILES['file']
+        decoded_file = file.read().decode('utf-8').splitlines()
         reader = csv.DictReader(decoded_file)
-
+        file = pd.read_csv()
         try:
-            for row in reader:
-                # Convert birthdate to a datetime object
-                birthdate = datetime.strptime(row['birthdate'], '%d/%m/%Y').date()
+            # for row in reader:                
+            #     # Convert birthdate to a datetime object
+            #     birthdate = datetime.strptime(row['birthdate'], '%d/%m/%Y').date()
 
-                # Convert empty string to None for score field
-                score = int(row['score']) if row['score'].strip() else 0
+            #     # Convert empty string to None for score field
+            #     score = int(row['score']) if row['score'].strip() else 0
 
-                Student.objects.create(
-                    name=row['name'],
-                    birthdate=birthdate,
-                    score=score,
-                    grade=row['grade']
-                )
+            #     Student.objects.create(
+            #         name=row['name'],
+            #         birthdate=birthdate,
+            #         score=score,
+            #         grade=row['grade']
+            #     )
+            if file.name.endswith('.csv'):
+                df = pd.read_csv(file, nrows=10)
+            elif file.name.endswith('.xls') or file.name.endswith('.xlsx'):
+                df = pd.read_excel(file, nrows=10)
+            else:
+                raise ValueError('Unsupported file format')
+
+            # Get the table name from the file
+            table_name = file.name.split('.')[0].replace(' ', '_').lower()
+            table_name = table_name.capitalize().replace('_', ' ')
+            
+            if apps.all_models['dynamic_data'].get(table_name):
+                model = apps.all_models['your_app_name'][table_name]
+                existing_fields = [field.name for field in model._meta.get_fields()]
+
+                for column in df.columns:
+                    field_type = check_type(df[column].iloc[0])  # Check data type of first row
+                    if column not in existing_fields:
+                        field = getattr(models, field_type)(null=True, blank=True)
+                        model.add_to_class(column, field)
+
+                model.save()
+            else:
+                fields = {
+                    column: getattr(models, check_type(df[column].iloc[0]))(null=True, blank=True)
+                    for column in df.columns
+                }
+                new_model = type(table_name, (models.Model,), fields)
+                new_model._meta.db_table = table_name.lower().replace(' ', '_')
+                new_model.save()
 
             return JsonResponse({'message': 'CSV data imported successfully'}, status=200)
         except Exception as e:
@@ -73,4 +107,30 @@ def upload_csv(request):
 
     return JsonResponse({'error': 'No file provided'}, status=400)
 
+def is_valid_date(date_str):
+    """lambda function for checking date validation
 
+    Args:
+        date_str (str): Date String
+
+    Returns:
+        Bool: is_valid_date
+    """
+    try:
+        parsed_date = parser.parse(date_str)
+        return True if parsed_date else False
+    except (ValueError, OverflowError):
+        return False
+
+def check_type(obj):
+    """Check the type of the input object."""
+    if is_valid_date(obj):
+        return 'DateField'
+    elif re.match(r'^-?\d+$', obj):
+        return 'IntegerField'
+    elif re.match(r'^-?\d+\.\d+$', obj):
+        return 'FloatField'
+    elif obj.lower() in ('true', 'false'):
+        return 'BooleanField'
+    else:
+        return 'CharField'
